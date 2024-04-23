7cde6665312aa21e23278f9b1e4d23526253627e
MSI\GA2B <h1202315026@hiratagakuen.onmicrosoft.com>
2024-04-23 11:32:03 +0900

    キャンパスの設定をscalewithscaleにした

---
+++ Assets/森本/スクリプト/ToHome.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PushButton()
+    {
+        Invoke("LoadScene", 3.0f);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
