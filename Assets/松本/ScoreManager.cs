d2ef22743e95e41c2aa9f2e94f37cb67892ed615
DESKTOP-U0TU0SG\GA2B <h1202315010@hiratagakuen.onmicrosoft.com>
2024-05-13 15:40:11 +0900

    GameManager作成＋スコア関係完成

--- Assets/松本/ScoreManager.cs
+++ Assets/松本/ScoreManager.cs
@@ -10,7 +10,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        score_num = 0;
     }
 
     // Update is called once per frame
