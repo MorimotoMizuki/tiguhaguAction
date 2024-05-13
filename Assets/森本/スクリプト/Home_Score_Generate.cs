ad48d2c9c030c82397a848d7f40f0b24d11e8b1a
DESKTOP-3GUQ93R\GA2B <h1202315026@hiratagakuen.onmicrosoft.com>
2024-05-13 15:41:09 +0900

    ホーム画面のクリアのスコアに応じたグラフィックをだすプログラム作成

--- Assets/森本/スクリプト/Home_Score_Generate.cs
+++ Assets/森本/スクリプト/Home_Score_Generate.cs
@@ -10,14 +10,16 @@ public class Home_Score_Generate : MonoBehaviour
     public GameObject Silver;
     public GameObject Bronze;
 
-    //スコアを呼び出す
-    ScoreManager scoreManager;
-    private int MyScore; //呼び出した変数を格納する変数
+    //親を指定
+    public Transform Parent;
+
+    //呼び出したスコアを格納する変数
+    private int MyScore;
 
     //各ステージのMAXScoreを格納する変数
-    private int Stage1_MAX = 11500;
-    private int Stage2_MAX = 10000;
-    private int Stage3_MAX = 10000;
+    public int Stage1_MAX = 11500;
+    public int Stage2_MAX = 10000;
+    public int Stage3_MAX = 10000;
 
     //各ステージがパーフェクトでクリアしたか判定するフラグ
     private bool Stage1_Perfect_flag = false;
@@ -27,26 +29,68 @@ public class Home_Score_Generate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MyScore = scoreManager.score_num;//呼び出したscore_numを格納
+        //ステージ１
+        //マイスコアにスコアを格納
+        MyScore = PlayerPrefs.GetInt("STAGE1SCORE", -1);
 
-        //ステージ１がパーフェクト
-        if (Stage1_Perfect_flag)
+        if (Stage1_Perfect_flag)//ステージ１がパーフェクト
         {
-            Instantiate(Perfect, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(Perfect, new Vector3(230f, 410f, 0f), Quaternion.identity, Parent);
         }
         else if (MyScore <= Stage1_MAX && MyScore >= Stage1_MAX - 3000)//金
         {
-            Instantiate(Gold, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(Gold, new Vector3(230f, 410f, 0f), Quaternion.identity, Parent);
         }
-        else if(MyScore <= Stage1_MAX - 3000 && MyScore >= Stage1_MAX - 8000)//銀
+        else if (MyScore <= Stage1_MAX - 3000 && MyScore >= Stage1_MAX - 8000)//銀
         {
-            Instantiate(Silver, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(Silver, new Vector3(230f, 410f, 0f), Quaternion.identity, Parent);
         }
-        else if(MyScore <= Stage1_MAX - 8000 && MyScore >= 0)//銅
+        else if (MyScore <= Stage1_MAX - 8000 && MyScore >= 0)//銅
         {
-            Instantiate(Bronze, new Vector3(0, 0, 0), Quaternion.identity);
+            Instantiate(Bronze, new Vector3(230f, 410f, 0f), Quaternion.identity, Parent);
         }
 
+        //ステージ２
+        //マイスコアにスコアを格納
+        MyScore = PlayerPrefs.GetInt("STAGE2SCORE", -1);
+
+        if (Stage2_Perfect_flag)//ステージ２がパーフェクト
+        {
+            Instantiate(Perfect, new Vector3(400f, 260f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage2_MAX && MyScore >= Stage2_MAX - 3000)//金
+        {
+            Instantiate(Gold, new Vector3(400f, 260f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage2_MAX - 3000 && MyScore >= Stage2_MAX - 8000)//銀
+        {
+            Instantiate(Silver, new Vector3(400f, 260f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage2_MAX - 8000 && MyScore >= 0)//銅
+        {
+            Instantiate(Bronze, new Vector3(400f, 260f, 0f), Quaternion.identity, Parent);
+        }
+
+        //ステージ３
+        //マイスコアにスコアを格納
+        MyScore = PlayerPrefs.GetInt("STAGE3SCORE", -1);
+
+        if (Stage3_Perfect_flag)//ステージ３がパーフェクト
+        {
+            Instantiate(Perfect, new Vector3(580f, 115f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage3_MAX && MyScore >= Stage3_MAX - 3000)//金
+        {
+            Instantiate(Gold, new Vector3(580f, 115f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage3_MAX - 3000 && MyScore >= Stage3_MAX - 8000)//銀
+        {
+            Instantiate(Silver, new Vector3(580f, 115f, 0f), Quaternion.identity, Parent);
+        }
+        else if (MyScore <= Stage3_MAX - 8000 && MyScore >= 0)//銅
+        {
+            Instantiate(Bronze, new Vector3(580f, 115f, 0f), Quaternion.identity, Parent);
+        }
     }
 
     // Update is called once per frame
