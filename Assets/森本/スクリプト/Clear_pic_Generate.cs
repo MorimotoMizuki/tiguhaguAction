fd6475d4c631424644770608a110cae2aeea1908
DESKTOP-M30FNO0\GA2B <h1202315026@hiratagakuen.onmicrosoft.com>
2024-05-16 16:37:48 +0900

    ゲームクリア画面のクリア演出の花火の調整、ゲーミング誉め言葉とメダルの位置調整完了

--- Assets/森本/スクリプト/Clear_pic_Generate.cs
+++ Assets/森本/スクリプト/Clear_pic_Generate.cs
@@ -20,6 +20,16 @@ public class Clear_pic_Generate : MonoBehaviour
     public GameObject Great;
     public GameObject Good;
 
+    //花火を入れる変数
+    public GameObject Hanabi_Yellow;
+    public GameObject Hanabi_Green;
+    public GameObject Hanabi_Blue;
+    public GameObject Hanabi_Red;
+
+    //花火の間隔
+    int interval = -25;
+    int random_collar = 0;
+
     //親を指定
     public Transform Parent;
 
@@ -31,19 +41,26 @@ public class Clear_pic_Generate : MonoBehaviour
     private int Stage2_MAX = 10000;
     private int Stage3_MAX = 10000;
 
+    //一回だけ処理をするための変数
+    private bool once_clear;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        once_clear = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void ClearDisplay()//ゲームをクリアしたら呼び出す
+    {
         //ゲームクリアしたら
-        if(playerController.isClear == true)
+        if (playerController.isClear == true && once_clear == true)
         {
-            Debug.Log(GameManager.stage_num);
             //ステージ１
             //マイスコアにスコアを格納
             if (GameManager.stage_num == 1)//ステージ１だったら
@@ -52,27 +69,106 @@ public class Clear_pic_Generate : MonoBehaviour
 
                 if (GameManager.stage1_P)//ステージ１がパーフェクト
                 {
-                    Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);//メダル
-                    Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-                    Debug.Log("Stage1");
+                    Instantiate(Perfect, new Vector3(371f, 202f, 0f), Quaternion.identity, Parent);//メダル
+                    Instantiate(Congratulations, new Vector3(377.8f - 15f, 316.5f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+
+                    //花火 10 5
+                    for (int i = 0; i < 10; i++, interval += 5)
+                    {
+                        random_collar = Random.Range(1, 5);//1～4でランダム
+                        switch (random_collar)
+                        {
+                            case 1:
+                                Instantiate(Hanabi_Yellow, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 2:
+                                Instantiate(Hanabi_Blue, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 3:
+                                Instantiate(Hanabi_Red, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 4:
+                                Instantiate(Hanabi_Green, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                        }
+                    }
                 }
                 else if (MyScore <= Stage1_MAX && MyScore >= Stage1_MAX - 3000)//金
                 {
-                    Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                    Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-                    Debug.Log("Stage1");
+                    Instantiate(Gold, new Vector3(371f, 202f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Excellent, new Vector3(377.8f -15f, 316.5f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+
+                    //花火 6 8
+                    for (int i = 0; i < 6; i++, interval += 8)
+                    {
+                        random_collar = Random.Range(1, 5);//1～4でランダム
+                        switch (random_collar){
+                            case 1:
+                                Instantiate(Hanabi_Yellow, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 2:
+                                Instantiate(Hanabi_Blue, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 3:
+                                Instantiate(Hanabi_Red, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 4:
+                                Instantiate(Hanabi_Green, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                        }
+                    }
                 }
                 else if (MyScore <= Stage1_MAX - 3000 && MyScore >= Stage1_MAX - 8000)//銀
                 {
-                    Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                    Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-                    Debug.Log("Stage1");
+                    Instantiate(Silver, new Vector3(371f, 202f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Great, new Vector3(377.8f - 15f, 316.5f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+
+                    //花火 4 14
+                    for (int i = 0; i < 4; i++, interval += 14)
+                    {
+                        random_collar = Random.Range(1, 5);//1～4でランダム
+                        switch (random_collar)
+                        {
+                            case 1:
+                                Instantiate(Hanabi_Yellow, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 2:
+                                Instantiate(Hanabi_Blue, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 3:
+                                Instantiate(Hanabi_Red, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 4:
+                                Instantiate(Hanabi_Green, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                        }
+                    }
                 }
                 else if (MyScore <= Stage1_MAX - 8000 && MyScore >= 0)//銅
                 {
-                    Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                    Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-                    Debug.Log("Stage1");
+                    Instantiate(Bronze, new Vector3(371f, 202f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Good, new Vector3(377.8f - 15f, 316.5f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+
+                    //花火 3 20
+                    for (int i = 0; i < 3; i++, interval += 20)
+                    {
+                        random_collar = Random.Range(1, 5);//1～4でランダム
+                        switch (random_collar)
+                        {
+                            case 1:
+                                Instantiate(Hanabi_Yellow, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 2:
+                                Instantiate(Hanabi_Blue, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 3:
+                                Instantiate(Hanabi_Red, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                            case 4:
+                                Instantiate(Hanabi_Green, new Vector3(playerController.transform.position.x + interval, playerController.transform.position.y, playerController.transform.position.z + 30f), Quaternion.identity);
+                                break;
+                        }
+                    }
                 }
             }
             //ステージ２
@@ -129,6 +225,11 @@ public class Clear_pic_Generate : MonoBehaviour
                     Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
                 }
             }
+
+            //一回だけ呼び出す変数をfalseにする
+            once_clear = false;
         }
+
     }
 }
+
