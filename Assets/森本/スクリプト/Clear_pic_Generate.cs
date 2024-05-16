79f855886d46e09afaebcfe2588c10af20f0af73
DESKTOP-3GUQ93R\GA2B <h1202315026@hiratagakuen.onmicrosoft.com>
2024-05-16 09:45:07 +0900

    クリア画面作成中

--- Assets/森本/スクリプト/Clear_pic_Generate.cs
+++ Assets/森本/スクリプト/Clear_pic_Generate.cs
@@ -6,6 +6,7 @@ public class Clear_pic_Generate : MonoBehaviour
 {
     //各ステージがパーフェクトであるかを判定する変数を呼び出すための宣言
     public GameManager gameManager;
+    public PlayerController playerController;
 
     //メダルのプレハブオブジェクトを入れるための変数
     public GameObject Perfect;
@@ -39,87 +40,90 @@ public class Clear_pic_Generate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ステージ１
-        //マイスコアにスコアを格納
-        if (gameManager.stage_num == 1)//ステージ１だったら
+        //ゲームクリアしたら
+        if(playerController.isClear == true)
         {
-            MyScore = PlayerPrefs.GetInt("STAGE1SCORE", -1);
+            //ステージ１
+            //マイスコアにスコアを格納
+            if (gameManager.stage_num == 1)//ステージ１だったら
+            {
+                MyScore = PlayerPrefs.GetInt("STAGE1SCORE", -1);
 
-            if (gameManager.stage1_P)//ステージ１がパーフェクト
-            {
-                Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);//メダル
-                Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                if (gameManager.stage1_P)//ステージ１がパーフェクト
+                {
+                    Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);//メダル
+                    Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage1_MAX && MyScore >= Stage1_MAX - 3000)//金
+                {
+                    Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage1_MAX - 3000 && MyScore >= Stage1_MAX - 8000)//銀
+                {
+                    Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage1_MAX - 8000 && MyScore >= 0)//銅
+                {
+                    Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
             }
-            else if (MyScore <= Stage1_MAX && MyScore >= Stage1_MAX - 3000)//金
+            //ステージ２
+            //マイスコアにスコアを格納
+            else if (gameManager.stage_num == 2)//ステージ２だったら
             {
-                Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                MyScore = PlayerPrefs.GetInt("STAGE2SCORE", -1);
+
+                if (gameManager.stage2_P)//ステージ２がパーフェクト
+                {
+                    Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage2_MAX && MyScore >= Stage2_MAX - 3000)//金
+                {
+                    Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage2_MAX - 3000 && MyScore >= Stage2_MAX - 8000)//銀
+                {
+                    Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage2_MAX - 8000 && MyScore >= 0)//銅
+                {
+                    Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
             }
-            else if (MyScore <= Stage1_MAX - 3000 && MyScore >= Stage1_MAX - 8000)//銀
+            //ステージ３
+            //マイスコアにスコアを格納
+            else if (gameManager.stage_num == 3)//ステージ３だったら
             {
-                Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage1_MAX - 8000 && MyScore >= 0)//銅
-            {
-                Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                MyScore = PlayerPrefs.GetInt("STAGE3SCORE", -1);
+
+                if (gameManager.stage3_P)//ステージ３がパーフェクト
+                {
+                    Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage3_MAX && MyScore >= Stage3_MAX - 3000)//金
+                {
+                    Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage3_MAX - 3000 && MyScore >= Stage3_MAX - 8000)//銀
+                {
+                    Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
+                else if (MyScore <= Stage3_MAX - 8000 && MyScore >= 0)//銅
+                {
+                    Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
+                    Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
+                }
             }
         }
-        //ステージ２
-        //マイスコアにスコアを格納
-        else if (gameManager.stage_num == 2)//ステージ２だったら
-        {
-            MyScore = PlayerPrefs.GetInt("STAGE2SCORE", -1);
-
-            if (gameManager.stage2_P)//ステージ２がパーフェクト
-            {
-                Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage2_MAX && MyScore >= Stage2_MAX - 3000)//金
-            {
-                Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage2_MAX - 3000 && MyScore >= Stage2_MAX - 8000)//銀
-            {
-                Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage2_MAX - 8000 && MyScore >= 0)//銅
-            {
-                Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-        }
-        //ステージ３
-        //マイスコアにスコアを格納
-        else if (gameManager.stage_num == 3)//ステージ３だったら
-        {
-            MyScore = PlayerPrefs.GetInt("STAGE3SCORE", -1);
-
-            if (gameManager.stage3_P)//ステージ３がパーフェクト
-            {
-                Instantiate(Perfect, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Congratulations, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage3_MAX && MyScore >= Stage3_MAX - 3000)//金
-            {
-                Instantiate(Gold, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Excellent, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage3_MAX - 3000 && MyScore >= Stage3_MAX - 8000)//銀
-            {
-                Instantiate(Silver, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Great, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-            else if (MyScore <= Stage3_MAX - 8000 && MyScore >= 0)//銅
-            {
-                Instantiate(Bronze, new Vector3(470f, 160f, 0f), Quaternion.identity, Parent);
-                Instantiate(Good, new Vector3(470f, 400f, 0f), Quaternion.identity, Parent);//ゲーミング誉め言葉
-            }
-        }
-
     }
 }
